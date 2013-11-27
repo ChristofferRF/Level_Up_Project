2ee7ca37b6c386a373fb782630a2a5d50b0820e1
Christoffer Finn Nielsen <chromecrf@gmail.com>
2013-11-27 10:52:34 +0100

    Tilføjet spike til Entity framwork del 1. opret recipe tabel

---
+++ Spikes/EntityFrameworkSpike/Controller/RecipeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using System.Data.Entity;
+
+namespace Controller
+{
+    /// side note - Install Entity Framework in all projects and define the connectionStrings in all the App.config across all projects
+    public class RecipeController
+    {
+        public RecipeController()
+        {
+
+        }
+
+        public void AddRecipeToDb(string title, string description)
+        {
+            //using statement to reference and access the database
+            //using will dro the connection after use
+            using (var db = new DataAccessContext())
+            {
+                Recipe rec = new Recipe
+                {
+                    Title = title,
+                    Description = description,
+                };
+
+                db.Recipes.Add(rec); //Add content to the database
+                db.SaveChanges();//Save the changes
+            }
+
+        }
+    }
+}
