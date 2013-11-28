4adf5a3ab0894fe840511cbcad4883d158f56ed7
Ronnie Hemmingsen <ronnie.hemmingsen@gmail.com>
2013-11-28 21:00:09 +0100

    tænkte jeg lige ville comitte clienten også... døøøhhh.

---
+++ Spikes/REST/RESTtest/RESTClient/client.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="client.aspx.cs" Inherits="RESTClient.client" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label Text="get person JSON:" runat="server"></asp:Label>
+        <br />
+        <asp:TextBox ID="getPersonJSONBox" runat="server" Height="106px" Width="307px"></asp:TextBox>
+
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ Spikes/REST/RESTtest/RESTClient/client.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.ServiceModel.Web;
+using RESTClient.Service;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace RESTClient
+{
+    public partial class client : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //test();
+            test2();
+        }
+
+        private void test()
+        {
+            WebChannelFactory<IPersonService> factory =
+                new WebChannelFactory<IPersonService>(new Uri("http://localhost:7976/PersonService.svc/rest/"));
+            IPersonService channel = factory.CreateChannel();
+            PersonModel person = channel.JSONPersonObject();
+
+            getPersonJSONBox.Text = person.Name + " - " + person.Age;
+        }
+
+        private void test2()
+        {
+            string text = "";
+            string url = string.Format("http://localhost:7976/PersonService.svc/rest/jsonpostobject/{0}", "Hans");
+            string urlEasy = string.Format("http://localhost:7976/PersonService.svc/rest/json/{0}", 23);
+            var request = WebRequest.Create(url);
+            Debug.WriteLine(url);
+
+
+            var response = (HttpWebResponse)request.GetResponse();
+            if (response.GetResponseStream() != null)
+            {
+                using (var sr = new StreamReader(response.GetResponseStream()))
+                {
+                    text = sr.ReadToEnd();
+                }
+            }
+
+            //getPersonJSONBox.Text = text;
+
+            PersonModel jsonObj = JsonConvert.DeserializeObject<PersonModel>(text);
+            getPersonJSONBox.Text = jsonObj.Name + " - " + jsonObj.Age;
+            
+        }
+    }
+}
