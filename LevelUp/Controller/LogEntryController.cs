4a92be468e471a850e7ad77eb929f8cd10572452
TokeOlsen <saxit_devil@hotmail.com>
2013-12-03 09:15:59 +0100

    LogEntry database oprettet

---
+++ LevelUp/Controller/LogEntryController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+using System.Data.Entity;
+
+namespace Controller
+{
+    public class LogEntryController
+    {
+        public LogEntryController()
+        {
+            
+        }
+
+        public void AddEntryToDb(string typeOfExcercise, string distance, int minutes)
+        {
+            using (var db = new DataAccessContext())
+            {
+                LogEntry log = new LogEntry
+                {
+                    TypeOfExcercise = typeOfExcercise,
+                    Distance = distance,
+                    Minutes = minutes,
+                };
+
+                db.LogEntries.Add(log);
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ LevelUp/DataAccess/LogEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class LogEntry
+    {
+        public string TypeOfExcercise { get; set; }
+        public string Distance { get; set; }
+        public int Minutes { get; set; }
+    }
+}
