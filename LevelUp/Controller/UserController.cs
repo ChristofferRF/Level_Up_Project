8d092b5f8184f1b8251f23b8de6a2d51c797bb1e
TokeOlsen <saxit_devil@hotmail.com>
2013-12-04 12:01:29 +0100

    User controller, db etc påbegyndt

---
+++ LevelUp/Controller/UserController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+using System.Data.Entity;
+
+namespace Controller
+{
+    public class UserController
+    {
+        public UserController()
+        {
+
+        }
+
+        public User AddUserToDb(string username, string password, string name, int age, int weight, int height)
+        {
+            User demoUser = new User();
+            int result = -1;
+
+            using (var db = new DataAccessContext())
+            {
+                User user = new User
+                {
+                    Username = username,
+                    Password = password,
+                    Name = name,
+                    Age = age,
+                    Weight = weight,
+                    Height = height,
+
+                    Logs = new List<LogEntry>
+                    {
+                        new LogEntry {TypeOfExcercise = "Running", Distance = "1,5km", Minutes = 15}
+                    }
+                };
+
+                db.Users.Add(user);
+                result = db.SaveChanges();
+            }
+
+            if (result != -1)
+                return demoUser;
+            else
+                return demoUser;
+        }
+
+    }
+}
