12d9f47df3c5381e4ac64bdf9f705ea58116ca38
Christoffer Finn Nielsen <chromecrf@gmail.com>
2013-12-04 10:43:37 +0100

    Lavet client GUI

--- LevelUp/Client/WebClient.aspx.cs
+++ LevelUp/Client/WebClient.aspx.cs
@@ -4,14 +4,29 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Diagnostics;
 
 namespace Client
 {
     public partial class WebClient : System.Web.UI.Page
     {
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            ShowText();
+        }
 
+        private void ShowText()
+        {
+            ExcerciseLabel.Text = StringValues.EXCERCISELABEL;
+            DistanceLabel.Text = StringValues.DISTANCELABEL;
+            TimeLabel.Text = StringValues.TIMELABEL;
+            CreateLogButton.Text = StringValues.CREATELOGBUTTON;
+        }
+
+        protected void CreateLog_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("succes you pushed the button");
         }
     }
 }
