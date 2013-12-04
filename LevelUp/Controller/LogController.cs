d4ca2c5b136108d503bf68eee905bca88783ead9
RasmusDM79 <rlillienskjold@gmail.com>
2013-12-04 12:48:35 +0100

    LoginGUI - rmlp

---
+++ LevelUp/Controller/LogController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+    public class LogController
+    {
+        // Constructor
+        public LogController()
+        {
+        }
+
+        public void createLog(string username)
+        {
+        }
+ 
+        public List<string> getLogs(string username)
+        {
+            return null;
+        }
+
+        public void addLog(string username)
+        {
+        }
+    }
+}
--- LevelUp/DataAccess/Class1.cs
+++ LevelUp/DataAccess/Class1.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace DataAccess
 {
-    public class Class1
+    class Class1
     {
-
     }
 }
