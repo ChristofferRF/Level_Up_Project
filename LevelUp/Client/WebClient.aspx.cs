cb5cb17ab04b90696b9e9470ff167ac333030222
RasmusDM79 <rlillienskjold@gmail.com>
2013-12-05 11:40:06 +0100

    input validation added  webclient - rmlp

--- LevelUp/Client/WebClient.aspx.cs
+++ LevelUp/Client/WebClient.aspx.cs
@@ -7,6 +7,7 @@ using System.Web.UI.WebControls;
 using System.Diagnostics;
 using DataAccess;
 using Client.App_Code;
+using System.Text.RegularExpressions;
 
 namespace Client
 {
@@ -28,15 +29,29 @@ namespace Client
 
         protected void CreateLog_Click(object sender, EventArgs e)
         {
-            LogEntry log = new LogEntry();
-            log.LogEntryId = 0;
-            log.TypeOfExcercise = excerciseTextBox.Text;
-            log.Distance = distanceTextBox.Text;
-            log.Minutes = Convert.ToInt32(MinutesTextBox.Text);
+            if (IsValidActitity(excerciseTextBox.Text) &&
+                IsValidDistance(distanceTextBox.Text) &&
+                IsvalidTime(HoursTextBox.Text) &&
+                IsvalidTime(MinutesTextBox.Text) &&
+                IsvalidTime(SecondsTextBox.Text))
+            {
+                LogEntry log = new LogEntry();
+                log.LogEntryId = 0;
+                log.TypeOfExcercise = excerciseTextBox.Text;
+                log.Distance = distanceTextBox.Text;
+                log.Minutes = Convert.ToInt32(MinutesTextBox.Text);
 
-            log = EntryCalls.AddLogEntry(log);
-            UpdateFields(log);
-            Debug.WriteLine("succes you pushed the button");
+                log = EntryCalls.AddLogEntry(log);
+                UpdateFields(log);
+                Debug.WriteLine("succes you pushed the button");
+            }
+            else
+            {
+                // Pop up some dialog + accumulate error message
+                string display = "Fejl i Input";
+                ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + display + "');", true);
+            }
+            
         }
 
         private void UpdateFields(LogEntry log)
@@ -45,6 +60,34 @@ namespace Client
             distanceTextBox.Text = log.Distance;
             MinutesTextBox.Text = log.Minutes.ToString();
             HoursTextBox.Text = "Grand succes!";
+        }
+
+        // Check for valid time input
+        public static bool IsvalidTime(string timeString)
+        {
+            if (timeString.Length < 3)
+            {
+                string intPattern = @"^[0-9]{0,2}$";
+                return Regex.IsMatch(timeString, intPattern);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Check for valid distance input
+        public static bool IsValidDistance(string distanceString)
+        {
+            string distancePattern = @"^-?[0-9]{0,4}([/./,][0-9]{1,3})$";
+            return Regex.IsMatch(distanceString, distancePattern);
+        }
+
+        // Check for valid input for activity
+        public static bool IsValidActitity(string activityString)
+        {
+            string activityPattern = @"^[a-zæøåA-ZÆØÅ ]*$";
+            return Regex.IsMatch(activityString, activityPattern);
         }
     }
 }
