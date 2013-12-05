3adca3c7670ac099ff41273743b96b4338034b56
RasmusDM79 <rlillienskjold@gmail.com>
2013-12-05 11:49:50 +0100

    loginclient inputvalidation added - rmlp

--- LevelUp/Client/LoginClient.aspx.cs
+++ LevelUp/Client/LoginClient.aspx.cs
@@ -6,6 +6,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using DataAccess;
 using Client.App_Code;
+using System.Text.RegularExpressions;
 
 namespace Client
 {
@@ -18,15 +19,35 @@ namespace Client
 
         protected void btnLogin_Click(object sender, EventArgs e)
         {
-            User user = new User();
-            user.Name = "Ronnie";
-            user.Password = "meh";
-            user.Username = "Kielgasten";
-            user.Weight = 85;
-            user.Height = 170;
+            if ((IsValidUserName(txtBoxUsername.Text)) && (txtBoxPassword.Text != null))
+            {
+                User user = new User();
+                user.Name = "Ronnie";
+                user.Password = "meh";
+                user.Username = "Kielgasten";
+                user.Weight = 85;
+                user.Height = 170;
 
-            user = UserCalls.AddUser(user);
-            txtBoxUsername.Text = user.Name;
+                user = UserCalls.AddUser(user);
+                txtBoxUsername.Text = user.Name;
+            }
+            else
+            {
+                string display = "Fejl i Input";
+                ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + display + "');", true);
+            }
+        }
+
+        public static bool IsValidUserName(string usernameString)
+        {
+            string usernamePattern = @"^[a-zA-Z0-9][-\w.]{0,22}([a-zA-Z\d0-9]|(?<![-.])_)$";
+            return Regex.IsMatch(usernameString, usernamePattern);
+        }
+
+        public static bool IsValidEmail(string emailString)
+        {
+            string emailPattern = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$";
+            return Regex.IsMatch(emailString, emailPattern);
         }
     }
 }
