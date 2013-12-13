f1c18eb331a069a5c09032a0301edb85af17475d
Christoffer Finn Nielsen <chromecrf@gmail.com>
2013-12-13 12:36:06 +0100

    Revert "fjernet merge errors på levelservice"
    This reverts commit 57215e1371cf5476ae377d8638e724347d1e566b.

--- LevelUp/RESTService/LevelService.svc.cs
+++ LevelUp/RESTService/LevelService.svc.cs
@@ -73,6 +73,7 @@ namespace RESTService
         }
 
         /// <summary>
+<<<<<<< HEAD
         /// kalder metode på UserControlleren som henter brugerens seneste 5 logs ud fra UserId
         /// </summary>
         /// <param name="userId"></param>
@@ -80,6 +81,7 @@ namespace RESTService
         public List<LogEntry> GetFiveLatestLogs(string userId)
         {
             return userCon.GetFiveLatestLogs(Convert.ToInt32(userId));
+=======
         /// Indsæt optjent xp på brugeren.
         /// </summary>
         /// <param name="userName"></param>
@@ -87,6 +89,7 @@ namespace RESTService
         public void UpdateUserXp(string userName, string earnedXp)
         {
             userCon.UpdateUserXP(userName, Convert.ToInt64(earnedXp));
+>>>>>>> 530998d2072e88997f6ba1efee8a3c7912118f97
         }
     }
 }
