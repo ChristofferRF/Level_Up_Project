93924748cc9ad15a348c36cd471c93e09ec72068
Christoffer Finn Nielsen <chromecrf@gmail.com>
2013-12-19 11:35:21 +0100

    Raw sql query til assign achievement

--- Scripts/GetAchievementsUser.sql
+++ Scripts/GetAchievementsUser.sql
@@ -1,7 +1,8 @@
-Select * from Achievements as a
+Select TOP 1 * from Achievements as a
 where AchievementId =
 (Select Achievement_AchievementId from UserAchievements
-where  User_UserId = 57 AND Achievement_AchievementId = a.AchievementId);
+where  User_UserId = 57 AND Achievement_AchievementId = a.AchievementId)
+Order By AchievementId DESC
 
 
 
